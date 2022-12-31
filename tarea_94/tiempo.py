ff27dccda83e1754132902f7b09a8ca386eb0d30
verogcortes <veromdp@live.com>
2022-12-31 12:47:20 +0100

    tareas 178 y 94

---
+++ tarea_94/tiempo.py
@@ -0,0 +1,12 @@
+def tierra(s1):
+    altura1=(1/2)*9.8*s1**2
+    return altura1
+def marte(s2):
+    altura2=(1/2)*3.7*s2**2
+    return altura2
+def jupiter (s3):
+    altura3=(1/2)*24.8*s3**2
+    return altura3
+print (tierra(5))
+print (marte(5))
+print (jupiter(5))
